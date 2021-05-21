7fa3406ac73024e02e0d9999c89b4475721df94c
nomaanhusain <32061948+nomaanhusain@users.noreply.github.com>
2021-05-21 11:23:54 +0530

    Update cowin.py

--- CowinFinder/cowin.py
+++ CowinFinder/cowin.py
@@ -1,14 +1,50 @@
-import urllib.request, json
+import json,sys
 import requests
 import time
 import smtplib
 starttime = time.time()
-# Find your district ID, steps included in README.md, put that in district_id in line 8 and enter desired date
-# Suppose your district_id is 654 and date is 21-05-2021, your url will look like the one in line 8
-url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=654&date=21-05-2021"
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'}
+
+# For District ID
+print("Enter your state(First letter should be capital)")
+stateName=input()
+urlState = "https://cdn-api.co-vin.in/api/v2/admin/location/states"
+
+statePage=requests.get(urlState,headers=headers)
+allStateData=statePage.json()
+stateData=allStateData['states']
+stateCode=0
+for i in stateData:
+    if(i['state_name']==stateName):
+        stateCode=i['state_id']
+        break
+
+if(stateCode==0):
+    print("Check state name and try again")
+    sys.exit()
+print("Enter your district name(As shown on cowin portal)")
+districtName=input()
+urlDistrict = f"https://cdn-api.co-vin.in/api/v2/admin/location/districts/{stateCode}"
+districtPage=requests.get(urlDistrict,headers=headers)
+allDistrictData=districtPage.json()
+districtData=allDistrictData['districts']
+districtId=0
+for i in districtData:
+    if(i['district_name']==districtName):
+        districtId=i['district_id']
+        print("Your district ID is : ",districtId)
+if(districtId==0):
+    print("Wrong District Name,Please try again")
+    sys.exit()
+
+# For session, to find slots
+print("Enter date (WARNING:Make sure it is in DD-MM-YYYY format)")
+date=input()
+url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={districtId}&date={date}"
+print(url)
 page=requests.get(url,headers=headers)
 
+# To send email
 def sendmail(email):
     print("Sending Mail.........")
     server=smtplib.SMTP('smtp.gmail.com',587)
@@ -28,20 +64,28 @@ def sendmail(email):
     server.quit()
 print("Enter your email")
 my_email=input()
-allData=page.json()
-newData=allData["sessions"]
+allSessionData=page.json()
+sessionData=allSessionData["sessions"]
 count = 0
+print()
+print("This will keep running and will check for slots every 1 minute, until manually stopped via Ctrl+C")
+print("An email will be sent as soon as an empty slot is found")
+print("To stop execution, press Ctrl+C")
+print()
 while True:
-    for i in newData:
+    for i in sessionData:
         # For 45+ type 45 instead of 18, for covishield replace COVAXIN with COVISHIELD
         # For dose 2 availability replace available_capacity_dose1 with available_capacity_dose2
         # to remove vaccine as parameter remove ""and i['vaccine']=='COVAXIN' "" from line 37 
         if(i['available_capacity_dose1']>0 and i['min_age_limit']==18 and i['vaccine']=='COVAXIN') :
             print("Name :",i['name'])
             print("Address :",i['address'])
+            print()
+            print('--------------------------------------------------------')
             count=count+1
-    print('--------------------------------------------------------')
+    
     if(count>0):
         sendmail(my_email)
+        print("To stop execution, press Ctrl+C")
     time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
